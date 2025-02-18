9929f37434f7df7eab6c288115a4cabfbb1253c4
CodeshShubh <codeshshubh@gmail.com>
2025-02-18 21:42:45 +0530

    complete2

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ const App = (props) => {
 
   return (
     <div className="App" id="main">
-        <p id="para" className={isVisible? 'show' : 'hide'}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-        <button id="click" onClick={()=>setisVisible(prev=>!prev)}>{isVisible ? 'hide' : 'show'}</button>
+        <p id="para" className={isVisible? 'hide' : 'show'}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        <button id="click" onClick={()=>setisVisible(prev=>!prev)}>{isVisible ? 'show' : 'hide'}</button>
     </div>
   );
 }
