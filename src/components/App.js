8657fe49e94f8adbc1589ad742d0e480393901f6
CodeshShubh <codeshshubh@gmail.com>
2025-02-18 21:38:31 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  const [isVisible, setisVisible] = useState(true)
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+        <p id="para" className={isVisible? 'show' : 'hide'}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        <button id="click" onClick={()=>setisVisible(prev=>!prev)}>{isVisible ? 'hide' : 'show'}</button>
     </div>
   );
 }
